fb1bff3537e38fa9075d65c004238d038cf2a135
arpit <arpit2003y@gmail.com>
2024-11-19 11:44:07 +0530

    chore(config):added_configurations

---
+++ app/(app)/(tabs)/explore.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function ExploreScreen() {
+    return (
+        <View className="flex-1 items-center justify-center text-white border-2">
+            <Text className="text-white"> this is explore tab</Text>
+        </View>
+    )
+}
---
+++ app/(app)/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function HomeScreen() {
+    return (
+        <View className="flex-1 items-center justify-center text-white h-screen">
+            <Text className="text-white">this is index tab</Text>
+        </View>
+    )
+}
